4c15b4b273aa9f164ddbe14fe2a1fb0030f29103
kimmzy-the-creative <98801030+kimmzy-the-creative@users.noreply.github.com>
2022-05-13 10:15:36 +0200

    Add files via upload

--- src/components/Functionality/SortByDropdown.js
+++ src/components/Functionality/SortByDropdown.js
@@ -1,30 +1,37 @@
 import React, { useEffect, useState, Fragment } from "react";
 
-const SortByDropdwon=(props)=>{
-  const [sortType, setSortType] = useState();
+const SortByDropdown=(props)=>{
 
-  useEffect(() => {
-    const sortArray = type => {
-      const types = {
-        size: 'size',
-        date: 'date',
-        price: 'price',
-      };
-      const sortProperty = types[type];
-      const sorted = [...products].sort((a, b) => b[sortProperty] - a[sortProperty]);
-      setData(sorted);
-    };
+  // useEffect(() => {
+  //   const sortArray = type => {
+  //     const types = {
+  //       size: 'size',
+  //       date: 'date',
+  //       price: 'price',
+  //     };
+  //     const sortProperty = types[type];
+  //     const sorted = [...products].sort((a, b) => b[sortProperty] - a[sortProperty]);
+  //     setData(sorted);
+  //   };
 
-    sortArray(sortType);
-  }, [sortType]);
+  //   sortArray(sortType);
+  // }, [sortType]);
 
-  return{
-    <select>
-      <option value="size">Size{props.product.size}</option>
-      <option value="date">Date{props.product.date}</option>
-      <option value="price">Price{props.product.price}</option>
-    </select>
+  const handleChange = (event) => {
+    props.setSortType(event.target.value);
   }
+
+  return (
+    <select value={props.sortType} onChange={handleChange}>
+      <option value="size">Size</option>
+      <option value="date">Date</option>
+      <option value="price">Price</option>
+
+      {/* <option value="size">Size{props.product.size}</option>
+      <option value="date">Date{props.product.date}</option>
+      <option value="price">Price{props.product.price}</option> */}
+    </select>
+  )
 }
 
-export default SortByDropdwon;
+export default SortByDropdown;
